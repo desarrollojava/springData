003a7870288b56d6aa4115e1aec38d744d7d4715
admin <admin@DESKTOP-AQQ97FA>
2018-05-22 19:18:24 -0500

    jdbc

---
+++ src/main/java/com/modelo/Persona.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modelo;
+
+//@Entity
+public class Persona {
+    private int idPersona;
+    private String  nombres;
+    private String  apellidos;
+    private String  direccionDomicilio;
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccionDomicilio() {
+        return direccionDomicilio;
+    }
+
+    public void setDireccionDomicilio(String direccionDomicilio) {
+        this.direccionDomicilio = direccionDomicilio;
+    }
+    
+    
+    
+}
